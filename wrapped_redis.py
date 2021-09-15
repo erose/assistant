015ffef94cacb52360d4fa27c438ddcdf751cda0
Eli Rose <really.eli@gmail.com>
2021-09-14 22:22:11 -0700

    Fix in-memory Redis implementation.

--- wrapped_redis.py
+++ wrapped_redis.py
@@ -1,12 +1,13 @@
+from typing import *
 import redis
 import os
 
 IN_MEMORY_REDIS = {}
 
-def get(key) -> str:
+def get(key) -> Optional[str]:
     if os.environ.get('REDIS_URL'):
         return redis.Redis.from_url(os.environ.get('REDIS_URL')).get(key)
-    return IN_MEMORY_REDIS[key]
+    return IN_MEMORY_REDIS.get(key, None)
 
 def set(key, value) -> None:
     if os.environ.get('REDIS_URL'):
