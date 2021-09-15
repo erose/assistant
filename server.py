70e9d7c8642005be83fc69c71764d08642964b59
Eli Rose <really.eli@gmail.com>
2021-09-14 23:50:31 -0700

    Set state again.

--- server.py
+++ server.py
@@ -49,6 +49,7 @@ def sms_reply():
             video_url = get_youtube_video_url()
             os.system(f"./download_single_or_multiple_videos.sh {video_url}")
             response_content = f"Downloaded {video_url}."
+            set_state(INITIAL_STATE)
         elif normalized_body == "n":
             set_state(INITIAL_STATE)
             # TODO
