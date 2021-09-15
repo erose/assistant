3ab8da0f682b6204bb079b8877d18537f4a1bce5
Eli Rose <really.eli@gmail.com>
2021-09-14 23:15:28 -0700

    Return string values from wrapped_redis.

--- wrapped_redis.py
+++ wrapped_redis.py
@@ -6,7 +6,9 @@ IN_MEMORY_REDIS = {}
 
 def get(key) -> Optional[str]:
     if os.environ.get('REDIS_URL'):
-        return redis.Redis.from_url(os.environ.get('REDIS_URL')).get(key)
+        value: bytes = redis.Redis.from_url(os.environ.get('REDIS_URL')).get(key)
+        return str(value)
+        
     return IN_MEMORY_REDIS.get(key, None)
 
 def set(key, value) -> None:
